d98442bb8d15c1a07f6dc3a5ea18eed22ba79b4e
Maciej Sikorski <maciejsikorski00@gmail.com>
2021-09-04 13:42:03 +0200

    Add CatsService methods

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -6,10 +6,92 @@ export class CatsService {
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
+
+
+
+
     this.cats.push(cat);
   }
 
+  update(cat: Cat) {
+
+
+
+
+
+  }
+
+  remove(catId: number) {
+
+
+
+
+
+  }
+
   findAll(): Cat[] {
+
+
+    
+    
     return this.cats;
   }
+
+  findOne(catId: number): Cat {
+
+
+
+
+    return this.cats[0]
+  }
+
+  adopt(cat: Cat, clinetId: number) {
+
+
+
+
+
+  }
+
+  return(cat: Cat) {
+
+
+
+
+
+  }
+
+  feed(cat: Cat) {
+
+
+
+
+
+
+  }
+
+  clean(cat: Cat) {
+
+
+
+
+
+  }
+
+  physicalExamination(cat: Cat) {
+
+  
+
+
+
+  }
+
+  changePlace(catId: number, placeId: number) {
+
+
+
+
+
+  }
+
 }
